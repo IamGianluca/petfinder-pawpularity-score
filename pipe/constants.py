608dea3a6e61d9d8d4de9943ab9dd75778c40f1c
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-10-23 22:49:59 -0400

    ML: create folds using Stratified K-fold strategy

---
+++ pipe/constants.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+# paths
+path = Path("/workspace")
+ckpts_path = path / "ckpts"
+data_path = path / "data"
+metrics_path = path / "metrics"
+
+orig_train_images_path = data_path / "train"
+orig_test_images_path = data_path / "test"
+
+# full paths
+train_labels_fpath = data_path / "train.csv"
+train_folds_fpath = data_path / "train_folds.csv"
+
+cfg_fpath = path / "params.yaml"
+
+# col names
+target_col = ["Pawpularity"]
