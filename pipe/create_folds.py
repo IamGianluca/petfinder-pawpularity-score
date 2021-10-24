599e198fe9a0d0915c2dfd09db54fea363ed68c0
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-10-24 11:22:19 -0400

    OPS: add image fpaths to train_folds.csv

--- pipe/create_folds.py
+++ pipe/create_folds.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import yaml
 from ml.params import load_cfg
 from sklearn.model_selection import StratifiedKFold
 
@@ -10,17 +9,22 @@ def split():
     cfg = load_cfg(fpath=str(constants.cfg_fpath), cfg_name="create_folds")
     df = pd.read_csv(constants.train_labels_fpath)
 
-    df["kfold"] = -1
-    df = df.sample(frac=1).reset_index(drop=True)
+    # add image fpaths
+    df["fpath"] = "../data/train/" + df.Id + ".jpg"
 
-    skf = StratifiedKFold(n_splits=cfg.k, shuffle=True, random_state=cfg.seed)
+    # split training data into 5 folds
+    df["kfold"] = -1
+    skf = StratifiedKFold(
+        n_splits=cfg.k,
+        shuffle=True,
+        random_state=cfg.seed,
+    )
     for fold_number, (train_idx, val_idx) in enumerate(
         skf.split(X=df, y=df.loc[:, constants.target_col])
     ):
         df.loc[val_idx, "kfold"] = fold_number
 
     print(df.groupby("kfold").mean())
-
     df.to_csv(constants.train_folds_fpath, index=False)
 
 
