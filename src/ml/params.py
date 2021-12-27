536f4909eb65025ff12c5563c5931da320c307e6
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-12-27 13:39:25 -0500

    ML: pass original images since we are resizing them already during data augmentation (#29)

--- src/ml/params.py
+++ src/ml/params.py
@@ -1,7 +1,13 @@
+from omegaconf.errors import ConfigAttributeError
 from pytorch_lightning.core.saving import load_hparams_from_yaml
 
 
 def load_cfg(fpath, cfg_name):
     cfg = load_hparams_from_yaml(config_yaml=fpath, use_omegaconf=True)
     cfg = cfg.get(cfg_name)
+    try:
+        if isinstance(cfg, str):
+            cfg.lr = float(cfg.lr)
+    except ConfigAttributeError:
+        pass
     return cfg
