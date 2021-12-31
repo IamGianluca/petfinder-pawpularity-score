8ac42c2a58f2bd9bfd8a3f6d87aba888bcb9c053
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-12-31 10:07:47 -0500

    ML: use extra data from dogs vs cats competition (#32)

--- pipe/constants.py
+++ pipe/constants.py
@@ -8,12 +8,14 @@ metrics_path = path / "metrics"
 
 train_images_path = data_path / "train"
 test_images_path = data_path / "test"
-extra_images_path = data_path / "extra"
+adoption_images_path = data_path / "extra"
+dogsvscats_images_path = data_path / "extra2"
 
 # full paths
 train_labels_fpath = data_path / "train.csv"
 train_deduped_fpath = data_path / "train_deduped.csv"
-extra_labels_fpath = data_path / "extra.csv"
+adoption_labels_fpath = data_path / "extra.csv"
+dogsvscats_labels_fpath = data_path / "extra2.csv"
 
 train_5folds_fpath = data_path / "train_5folds.csv"
 train_10folds_fpath = data_path / "train_10folds.csv"
